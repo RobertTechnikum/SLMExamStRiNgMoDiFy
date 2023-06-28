8b07f14afe7e68e94584da8670703c1f3b7b381f
Robert Murhamer <wi22b073@technikum-wien.at>
2023-06-28 19:36:24 +0200

    Initial commit

---
+++ src/main/java/com/example/SLMExamStRiNgMoDiFy/SlmExamStRiNgMoDiFyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SLMExamStRiNgMoDiFy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExamStRiNgMoDiFyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmExamStRiNgMoDiFyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SLMExamStRiNgMoDiFy/SlmExamStRiNgMoDiFyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLMExamStRiNgMoDiFy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamStRiNgMoDiFyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
